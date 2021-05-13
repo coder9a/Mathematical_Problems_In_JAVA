a2cc64ec7a67e00acf127fbd5399b57906e45318
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-05-13 23:11:29 +0530

    Add files via upload

---
+++ NumberOfPaths.java
@@ -0,0 +1,54 @@
+/**
+ * The problem is to count all the possible paths from top left to bottom right
+ * of a MxN matrix with the constraints that from each cell you can either move to right or down.
+ *
+ * Example 1:
+ *
+ * Input:
+ * M = 3 and N = 3
+ * Output: 6
+ * Explanation:
+ * Let the given input 3*3 matrix is filled
+ * as such:
+ * A B C
+ * D E F
+ * G H I
+ * The possible paths which exists to reach
+ * 'I' from 'A' following above conditions
+ * are as follows:ABCFI, ABEHI, ADGHI, ADEFI,
+ * ADEHI, ABEFI
+ *
+ *
+ * Example 2:
+ *
+ * Input:
+ * M = 2 and N = 8
+ * Output: 8
+ */
+public class NumberOfPaths
+{
+//    ---------------------------------------------------
+//    Time Complexity = O(2^n)
+//    public static int fact(int m, int n)
+//    {
+//        if(m==1 || n==1)
+//        {
+//            return 1;
+//        }
+//        else
+//        {
+//            return fact(m,n-1) + fact(m-1, n);
+//        }
+//    }
+//
+//    public static void main(String[] args)
+//    {
+//        int m=3, n=3;
+//        int x = fact(m,n);
+//        System.out.println(x);
+//    }
+
+//    ---------------------------------------------------
+//    Time Complexity = O(n)   ---->>> Using Dynamic Programming
+
+}
